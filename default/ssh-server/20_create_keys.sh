3d7284afb5b7acab79dee2d5493268125c573886
Chris Baron <cmbaron@gmail.com>
2023-04-20 10:53:18 -0400

    gracefully handle initial creation

--- default/ssh-server/20_create_keys.sh
+++ default/ssh-server/20_create_keys.sh
@@ -2,5 +2,5 @@
 
 microk8s status --wait-ready
 
-kubectl --namespace default delete secret ssh-keys
+kubectl --namespace default delete secret ssh-keys &>/dev/null || echo "no keys previously created"
 kubectl --namespace default create secret generic ssh-keys --from-file=authorized_keys
